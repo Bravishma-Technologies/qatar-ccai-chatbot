ad476530aabd865aab8fabfe3579e8d64154008c
ViveK <vivekn@bravishma.com>
2024-01-05 10:20:15 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { ToastContainer } from "react-toastify";
+
+import "./App.css";
+import Home from "./pages/Home";
+
+function App() {
+	const [user, setUser] = useState({});
+	const queryParameters = new URLSearchParams(window.location.search);
+	const userId = queryParameters.get("userId");
+	const jwtToken = queryParameters.get("jwtToken");
+	const name = queryParameters.get("name");
+
+	// console.log("name==> ", name);
+	// console.log("userId=> ", userId);
+	// console.log("jwtToken=> ", jwtToken);
+
+	useEffect(() => {
+		if (name && userId && jwtToken) {
+			setUser({ name, userId, jwtToken });
+			console.log("hi==> ", user);
+		}
+	}, []);
+
+	// useEffect(() => {
+	//   setTimeout(() => {
+	//     console.log("extd");
+	//     if (typeof window.AvMessagingSdk === "object") {
+	//       const avMessenger = window.AvMessagingSdk;
+	//       console.log("avMessenger==> ", avMessenger);
+	//       avMessenger.login(userId, jwtToken);
+	//     } else {
+	//       console.error("msg function not available.");
+	//     }
+	//   }, 5000);
+	// }, []);
+
+	return (
+		<div className="App">
+			<ToastContainer />
+			<Home user={user} setUser={setUser} />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+
+import NavBar from "../components/Navbar";
+import Footer from "../components/Footer/index";
+
+import LoginForm from "../components/LoginForm";
+// import NavBar from "../components/NavBar";
+import "./Home.css";
+
+import roundedRect from "../assets/roundedRect.svg";
+import bgImg from "../assets/images/bg.jpg";
+import logoImg from "../assets/images/logo.png";
+
+import Main from "../components/Main";
+import Login from "../components/Login";
+
+const Home = ({ user, setUser }) => {
+	const [userPresent, setUserPresent] = useState(false);
+	const [messengerReady, setMessengerReady] = useState(false);
+	// console.log("================z> ", user);
+	useEffect(() => {
+		if (Object.keys(user).length) {
+			setUserPresent(true);
+		}
+	}, [user]);
+
+	// console.log("userPresent====> ", userPresent);
+
+	useEffect(() => {
+		console.log("messengerReady--> ", messengerReady);
+		let msgInterval = null;
+
+		if (!messengerReady) {
+			msgInterval = setInterval(() => {
+				if (window.messengerReady) setMessengerReady(true);
+			}, 0);
+		}
+
+		if (messengerReady) {
+			clearInterval(msgInterval);
+		}
+
+		return () => {
+			clearInterval(msgInterval);
+		};
+	}, [messengerReady]);
+
+	useEffect(() => {
+		if (userPresent && messengerReady) {
+			const { userId, jwtToken } = user;
+			msgrLogin(userId, jwtToken);
+		}
+	}, [user, userPresent, messengerReady]);
+
+	return (
+		<div className="container-man">
+			<NavBar user={user} />
+			<div className="container-content">
+				{userPresent ? (
+					<Main />
+				) : (
+					<Login setUser={setUser} setUserPresent={setUserPresent} />
+				)}
+			</div>
+			<Footer />
+		</div>
+	);
+};
+
+function msgrLogin(userId, jwtToken) {
+	// console.log('userid==> ',userId," -- tokn -- ",jwtToken);
+	try {
+		const avMessenger = window.AvMessagingSdk;
+		console.log("avMessenger==> ", avMessenger);
+		avMessenger.login(userId, jwtToken);
+	} catch (error) {
+		console.log("msg function not available.");
+	}
+}
+
+export default Home;
